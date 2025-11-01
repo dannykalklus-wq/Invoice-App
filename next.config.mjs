2dc5ce50c7c7b901974f541bde89d16952d525ec
dannykalklus-wq <dannykalklus@gmail.com>
2025-11-01 12:08:34 +0000

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,6 @@
-```js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 };
+
 export default nextConfig;
-```
-
----
-
-### 3. `postcss.config.mjs`
-```js
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-};
-```
